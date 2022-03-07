0e5c8f49c5e0d86f443214a7f694a33ae207fb16
ProjetosDaEquipe <bmgames6@gmail.com>
2022-03-07 11:46:37 -0300

    First commit

---
+++ app/src/main/java/com/mrcant/projetosantanderdevweek/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mrcant.projetosantanderdevweek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
